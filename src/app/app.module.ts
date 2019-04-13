18b987f14ce0caa193e6ae671075b4b6e7083c35
Max Lutsenko <lutsenko.maxim@gmail.com>
2019-04-13 13:18:30 +0300

    Create get request and output projects in the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ApiService} from "./api.service";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
